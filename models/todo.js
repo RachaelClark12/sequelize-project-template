7c5e15178786efeaba801071f9778e082a6a76d4
Rachael Clark <rachael.clark@uconn.edu>
2019-11-09 13:35:53 -0500

    deploy

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   const Todo = sequelize.define('Todo', {
-    text: DataTypes.STRING,
-
+    text: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      },
+    },
     complete: DataTypes.BOOLEAN,
   });
   return Todo;
